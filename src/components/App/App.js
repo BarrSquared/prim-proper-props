66226ea2517b89b9da6a82f85a9b86103fece788
Russell <42157714+BarrSquared@users.noreply.github.com>
2021-09-15 14:17:45 -0500

    Initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+import Header from '../Header/Header.js';
+import Footer from '../Footer/Footer.js';
+import GuestList from '../GuestList/GuestList.js';
+import DinnerSupplies from '../DinnerSupplies/DinnerSupplies.js';
+import GuestForm from '../GuestForm/GuestForm.js';
+
+function App() {
+  let [guestList, setGuestList] = useState([]);
+  let [newGuestName, setNewGuestName] = useState('');
+  let [newGuestMeal, setNewGuestMeal] = useState('false');
+
+  //On load, get guests
+  useEffect(() => {
+    getGuests()
+  }, [])
+
+  const getGuests = () => {
+    axios.get('/guests')
+      .then(response => {
+        setGuestList(response.data)
+      })
+      .catch(err => {
+        alert('error getting guests');
+        console.log(err);
+      })
+  }
+
+
+  const addGuest = () => {
+    axios.post('/guests', { name: newGuestName, kidsMeal: newGuestMeal })
+      .then(response => {
+        // clear inputs
+        setNewGuestName('');
+        setNewGuestMeal(false);
+
+        getGuests();
+      })
+      .catch(err => {
+        alert('Error Adding Guest');
+        console.log(err);
+      })
+  };
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (newGuestName) {
+      addGuest();
+    }
+    else {
+      alert('The new guest needs a name!');
+    }
+  }
+
+  console.log(newGuestMeal)
+  return (
+    <div className="App">
+      <Header />
+      <h2>Party Leader</h2>
+      {guestList[0] && <h3>{guestList[0].name}</h3>}
+      <GuestForm guestList={guestList}/>
+      
+      <GuestList guestList={guestList}/>
+      <DinnerSupplies guestList={guestList} />
+      
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
